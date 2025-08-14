255e5c79e435cb955a3965ccfd193ed1e7d00bea
bpi-japonte2 <157174428+bpi-japonte2@users.noreply.github.com>
2025-08-14 15:33:00 +0000

    Update formatting

--- Remove-PreinstalledO365Apps.ps1
+++ Remove-PreinstalledO365Apps.ps1
@@ -8,6 +8,6 @@ ForEach ($App in $AppList) {
 		$OneNote = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq $AppName } | Select-Object -Property DisplayName,UninstallString
 	    $uninst = $OneNote.uninstallstring + " displaylevel=false"
 
-	Start-Process -WindowStyle Hidden cmd.exe -ArgumentList '/c', $uninst -Wait -PassThru
+	    Start-Process -WindowStyle Hidden cmd.exe -ArgumentList '/c', $uninst -Wait -PassThru
 	}
 }
